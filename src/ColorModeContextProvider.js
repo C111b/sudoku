d842b49fe4d0466fc9b63230fee4415d20f21a3b
Caley <caley.badillo@gmail.com>
2022-08-16 14:59:06 -0400

    information dialog w/ button integration

--- src/ColorModeContextProvider.js
+++ src/ColorModeContextProvider.js
@@ -22,6 +22,7 @@ export default function ColorModeContextProvider({children}) {
         createTheme({
             palette: {
                 mode,
+                
             },
         }),
         [mode],
--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,34 @@
-import { AppBar, IconButton, Toolbar, Typography, useTheme } from "@mui/material";
+import { useContext, useState } from "react";
+import {
+  AppBar,
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Divider,
+  IconButton,
+  Toolbar,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import { ColorModeContext } from "./ColorModeContextProvider.js";
 
 //icons
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
-import { useContext } from "react";
 
 export default function Header() {
-//context
+  //for info button
+  const [info, setInfo] = useState(true);
+
+  //context
   const theme = useTheme();
   const colorMode = useContext(ColorModeContext);
+
+  const handleInfo = () => {
+    return info ? setInfo(false) : setInfo(true);
+  };
 
   return (
     <AppBar position="static">
@@ -18,20 +36,53 @@ export default function Header() {
         <Typography component="div" sx={{ flexGrow: 1 }}>
           Sudoku
         </Typography>
-        <IconButton>
+        <IconButton onClick={handleInfo}>
           <HelpOutlineIcon />
         </IconButton>
+        <Dialog
+          fullWidth={true}
+          maxWidth={"sm"}
+          open={info}
+          onClose={handleInfo}
+        >
+          <DialogTitle sx={{ textAlign: "center" }}>How To Play</DialogTitle>
+          <DialogContent>
+            <DialogContentText
+              sx={{
+                color: "text.primary",
+                textAlign: "center",
+              }}
+            >
+              Complete the Sudoku.
+              <Divider sx={{ m: 2 }}></Divider>
+            </DialogContentText>
+            <DialogContentText
+                    sx={{
+                      color: "text.primary",
+                      fontSize: ".75rem",
+                      // ml: 5
+                    }}
+                  >Complete segments by filling in numbers from 1-9
+                  <br /> <br />
+                  Segments consist of: <br />
+                  - Rows <br />
+                  - Columns <br />
+                  - 3x3 Blocks <br /> <br />
+                  *No duplicate number should exist in a segment.
+                  </DialogContentText>
+          </DialogContent>
+        </Dialog>
         <IconButton
           sx={{ justifyContent: "flex-end", mr: 1 }}
           color="secondary"
-        //   edge="end"
-            onClick={colorMode.toggleColorMode}
+          //   edge="end"
+          onClick={colorMode.toggleColorMode}
         >
           {theme.palette.mode === "dark" ? (
             <Brightness7Icon />
           ) : (
-          <Brightness4Icon />
-          )} 
+            <Brightness4Icon />
+          )}
         </IconButton>
       </Toolbar>
     </AppBar>
